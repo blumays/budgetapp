9aca6c7de65f3da41c4dcebabebdf44ea0484a32
Blu Mays <bmays@dcpmidstream.com>
2019-10-27 12:12:58 -0600

    second

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const reasonInput = document.querySelector("#input-reason");
 const amountInput = document.querySelector("#input-amount");
 const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
-const expensesList = document.querySelector("expense");
+const expensesList = document.querySelector("#expenses-list");
+const totalExpensesOutput = document.querySelector("#total-expenses");
+
+let totalExpenses = 0;
+
+const clear = () => {
+  reasonInput.value = "";
+  amountInput.value = "";
+};
 
 confirmBtn.addEventListener("click", () => {
   const enteredReason = reasonInput.value;
@@ -15,5 +23,14 @@ confirmBtn.addEventListener("click", () => {
   ) {
     return;
   }
-  console.log(enteredReason, enteredAmount);
+  const newItem = document.createElement("ion-item");
+  newItem.textContent = enteredReason + ": $" + enteredAmount;
+
+  expensesList.appendChild(newItem);
+
+  totalExpenses += +enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses;
+  clear();
 });
+
+cancelBtn.addEventListener("click", clear);
